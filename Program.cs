ab94d46b25433a169ac9fe1ddbe2ddceaf9c5fe0
Iryna Krush <145108812+Ira-name@users.noreply.github.com>
2024-05-11 18:45:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+class Food
+{
+    protected string Name { get; set; }
+    protected double Price { get; set; }
+    public Food(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public virtual string GetName()
+    {
+        return Name;
+    }
+    public virtual double GetPrice()
+    {
+        return Price;
+    }
+}
+class FoodDecorator : Food
+{
+    protected Food DecoratedFood { get; set; }
+
+    public FoodDecorator(Food decoratedFood) : base("", 0)
+    {
+        DecoratedFood = decoratedFood;
+    }
+
+    public override string GetName()
+    {
+        return DecoratedFood.GetName();
+    }
+
+    public override double GetPrice()
+    {
+        return DecoratedFood.GetPrice();
+    }
+}
+class IngredientDecorator : FoodDecorator
+{
+    protected string IngredientName { get; set; }
+    protected double IngredientPrice { get; set; }
+
+    public IngredientDecorator(Food decoratedFood, string ingredientName, double ingredientPrice)
+        : base(decoratedFood)
+    {
+        IngredientName = ingredientName;
+        IngredientPrice = ingredientPrice;
+    }
+
+    public override string GetName()
+    {
+        return base.GetName() + ", " + IngredientName;
+    }
+
+    public override double GetPrice()
+    {
+        return base.GetPrice() + IngredientPrice;
+    }
+}
+
+class Burger : Food
+{
+    public Burger() : base("Burger", 5.99)
+    {
+    }
+}
+class Cheese : Food
+{
+    public Cheese() : base("Cheese", 1.50)
+    {
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Food burger = new Burger();
+        Food burgerWithCheese = new IngredientDecorator(burger, "Cheese", 1.50);
+        Console.WriteLine("You ordered: " + burgerWithCheese.GetName());
+        Console.WriteLine("Total cost: $" + burgerWithCheese.GetPrice());
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9_P
+{
+    using System;
+    class Window
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing window");
+        }
+    }
+    class ScrollingDecorator : Window
+    {
+        private readonly Window _decoratedWindow;
+
+        public ScrollingDecorator(Window decoratedWindow)
+        {
+            _decoratedWindow = decoratedWindow;
+        }
+
+        public override void Draw()
+        {
+            _decoratedWindow.Draw();
+            Console.WriteLine("Adding scrolling functionality");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Window simpleWindow = new Window();
+            simpleWindow.Draw();
+            Console.WriteLine();
+            Window scrollingWindow = new ScrollingDecorator(simpleWindow);
+            scrollingWindow.Draw();
+        }
+    }
+}
